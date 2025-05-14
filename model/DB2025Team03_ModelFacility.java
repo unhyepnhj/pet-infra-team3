688afdb31feaa7d2a2eef4a92c8ad9b84ef4b6c4
rileyRyu <rdh0827@gmail.com>
2025-05-14 20:32:40 +0900

    Add files via upload

--- model/DB2025Team03_ModelFacility.java
+++ model/DB2025Team03_ModelFacility.java
@@ -1,5 +1,77 @@
 package model;
 
 public class DB2025Team03_ModelFacility {
+    private int facilityId;
+    private String name;
+    private String address;
+    private String category;
+    private String openingHours;
 
+    /**
+     * @param facilityId 
+     * @param name     
+     * @param address    
+     * @param category    
+     * @param openingHours
+     */
+    public DB2025Team03_ModelFacility(int facilityId, String name, String address,
+                                      String category, String openingHours) {
+        this.facilityId = facilityId;
+        this.name = name;
+        this.address = address;
+        this.category = category;
+        this.openingHours = openingHours;
+    }
+
+    // Getter, Setter
+    public int getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(String openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    @Override
+    public String toString() {
+        return "DB2025Team03_ModelFacility{" +
+               "facilityId=" + facilityId +
+               ", name='" + name + '\'' +
+               ", address='" + address + '\'' +
+               ", category='" + category + '\'' +
+               ", openingHours='" + openingHours + '\'' +
+               '}';
+    }
 }
--- model/DB2025Team03_ModelFavorite.java
+++ model/DB2025Team03_ModelFavorite.java
@@ -1,5 +1,40 @@
 package model;
 
 public class DB2025Team03_ModelFavorite {
+    private int userId;
+    private int facilityId;
 
+    /**
+     * @param userId      
+     * @param facilityId 
+     */
+    public DB2025Team03_ModelFavorite(int userId, int facilityId) {
+        this.userId = userId;
+        this.facilityId = facilityId;
+    }
+
+    // Getter, Setter
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    @Override
+    public String toString() {
+        return "DB2025Team03_ModelFavorite{" +
+               "userId=" + userId +
+               ", facilityId=" + facilityId +
+               '}';
+    }
 }
