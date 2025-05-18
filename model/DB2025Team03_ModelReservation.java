d6f6255dc705740f28803576f571fb3d716475a7
laurenjung03 <141549828+laurenjung03@users.noreply.github.com>
2025-05-19 01:11:30 +0900

    Update DB2025Team03_ModelReservation.java

--- model/DB2025Team03_ModelReservation.java
+++ model/DB2025Team03_ModelReservation.java
@@ -1,5 +1,69 @@
 package model;
 
 public class DB2025Team03_ModelReservation {
+    private int reservationId;
+    private int userId;
+    private int facilityId;
+    private String date;
+    private String serviceType;
 
+    public DB2025Team03_ModelReservation(int reservationId, int userId, int facilityId, String date, String serviceType) {
+        this.reservationId = reservationId;
+        this.userId = userId;
+        this.facilityId = facilityId;
+        this.date = date;
+        this.serviceType = serviceType;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    @Override
+    public String toString() {
+        return "DB2025Team03_ModelReservation{" +
+                "reservationId=" + reservationId +
+                ", userId=" + userId +
+                ", facilityId=" + facilityId +
+                ", date='" + date + '\'' +
+                ", serviceType='" + serviceType + '\'' +
+                '}';
+    }
 }
+
